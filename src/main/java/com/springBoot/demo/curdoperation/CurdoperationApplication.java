d30786f23d20436ddd8887601b4bed3200e3cb45
Shubhendu Pal <shubhendupal66@gmail.com>
2022-01-12 14:39:05 +0530

    External server deployment config

--- src/main/java/com/springBoot/demo/curdoperation/CurdoperationApplication.java
+++ src/main/java/com/springBoot/demo/curdoperation/CurdoperationApplication.java
@@ -2,9 +2,17 @@ package com.springBoot.demo.curdoperation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CurdoperationApplication {
+public class CurdoperationApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(CurdoperationApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurdoperationApplication.class, args);
